bf94eff17ef90a1331da0043824f990342263c9b
Nithingowda021 <52652008+Nithingowda021@users.noreply.github.com>
2020-10-01 12:17:31 +0530

    Update the missing statement in given code

--- Program-1.c
+++ Program-1.c
@@ -1,9 +1,10 @@
 //Fill in the missing code in the below sample code which copies a given string into another string.
+// Instructions:-
 //1) Initially, read a string from the standard input device and write a loop to copy each character of given string into another string till the end of the string is reached.
 //2) Place '\0' at the end of the copied string.
 //3) Finally, the copied string is displayed on the screen.
 
-//Expected input & output:
+//Expected input & output:-
 //Enter a String-1: Nithin
 //The copied String-2 = Nithin
 
