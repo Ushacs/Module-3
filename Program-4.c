b80f5916c46c5e04c7234d4e83449814ebd7bf5d
Nithingowda021 <52652008+Nithingowda021@users.noreply.github.com>
2020-10-01 12:24:22 +0530

    Create Program-4.c

---
+++ Program-4.c
@@ -0,0 +1,11 @@
+// Create a C Program to Find the Frequency of Characters in a String.
+
+//Instructions:-
+// 1)String entered by the user is stored in a variable, then, the user is asked to enter the character whose frequency is to be found. This is stored in another variable.
+// 2)A for loop can be used to iterate over characters of the string. In each iteration, if the character in the string is equal to the character given by user, then count is increased by 1.
+// 3)Finally, the frequency stored in the count variable is printed.
+
+//Expected input & output:-
+//Enter a string: This college is awesome
+//Enter a character to find its frequency: e
+//Frequency of e = 4
