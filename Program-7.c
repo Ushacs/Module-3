c8160ed05af2d7d9335c774272f0589cad7398c4
Usha C S <35720624+Ushacs@users.noreply.github.com>
2020-10-01 11:57:52 +0530

    Create Program-7.c

---
+++ Program-7.c
@@ -0,0 +1,26 @@
+/*Instruction:
+Write a function int max_of_four(int a, int b, int c, int d) which reads four arguments and returns the greatest of them.
+Note:
+There is no built in max function in C. 
+Code that will be reused is often put in a separate function, e.g. int max(x, y) that returns the greater of the two values.
+Input Format
+Input will contain four integers 
+Output Format
+Print the greatest of the four integers.*/
+
+#include <stdio.h>
+#include <conio.h>
+
+/*
+Add function prototype here.
+*/
+
+int main() {
+    int a, b, c, d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    int ans = max_of_four(a, b, c, d);
+    printf("%d", ans);
+    
+    return 0;
+}
+
