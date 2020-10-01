0ba309414d8ace509fae18547686a7ba9645cc73
Nithingowda021 <52652008+Nithingowda021@users.noreply.github.com>
2020-10-01 12:25:53 +0530

    Create Program-5.c

---
+++ Program-5.c
@@ -0,0 +1,10 @@
+//Create a C Program to Remove all Characters in a String Except Alphabets
+
+//Instructions:-
+// 1)Read a string input from the user and stores in a variable. Then, use a for loop to iterate over characters of the string.
+// 2)If the character in a string is not an alphabet, it is removed from the string and the positions of the remaining characters are shifted to the left by 1 position.
+// 3)Finally, string will be printed which consists of only alphabets.
+
+//Expected input & output:-
+//Enter a string: N’ith242-=in
+//Output String = Nithin
