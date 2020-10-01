c5a3235ba80cae6019d2eee72bde5d2306bce89a
Usha C S <35720624+Ushacs@users.noreply.github.com>
2020-10-01 12:03:08 +0530

    Create Program-9.c

---
+++ Program-9.c
@@ -0,0 +1,28 @@
+/*Instruction: The below program is to read a one-dimensional array and print that array using functions.
+
+Fill in the missing code so that it produces the desired output.*/
+
+
+#include <stdio.h>
+void read(int [], int);
+void display(int [], int);
+void main() {
+	int a[10], n;
+	printf("Enter n value : ");
+	scanf("%d", &n);
+	read( ); // Write the arguments in the function call
+	display( ); // Write the arguments in the function call
+}
+void read( ) { // Write the arguments in the function definition
+	printf("Enter %d elements : ", n);
+	for ( ) { // Complete the code in for
+		scanf("%d", ); // Complete the statement
+	}
+}
+void display( ) { // Write the arguments in the function definition
+	printf("Elements in the array are : ");
+	for ( ) { // Complete the code in for
+		printf("%d ", ); // Complete the statement
+	}
+}
+
